147b882dfa458f5f5b58619a2fcbd6a5996865b3
Jonathan Braun <Jonathanrbraun@gmail.com>
2017-01-17 16:55:33 -0500

    Fix monsterS table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117194325) do
+ActiveRecord::Schema.define(version: 20170117214438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "monster", force: :cascade do |t|
+  create_table "monsters", force: :cascade do |t|
     t.string   "name",        null: false
     t.string   "source",      null: false
     t.string   "img",         null: false
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(version: 20170117194325) do
     t.string   "email"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
   end
 
 end
