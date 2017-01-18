45513beedeab09cee598d65ca500a218887ce47b
Suzi Curran <suzicurran@gmail.com>
2017-01-18 14:11:56 -0500

    failing feature test for adding a new monster

---
+++ spec/features/06_user_creates_a_new_monster_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+feature "users can add monsters" do
+  scenario "user adds new monster successfully" do
+
+    visit new_monster_path
+    expect(page).to have_content "Add a new monster!"
+
+    fill_in 'Name', with: "R.O.U.S."
+    fill_in 'Source', with: "http://princessbride.wikia.com/wiki/R.O.U.S."
+    fill_in 'Image', with: "http://princessbrideforever.com/site/wp-content/uploads/57_ROUS1.jpg"
+    fill_in 'Description', with: "Rodents Of Unusual Size are giant mouse-like creatures that Westley and Buttercup encounter in the Fire Swamp. They are dangerous and don't attack or strike unless they are not out-numbered or smell blood."
+    fill_in 'Email Address', with: "rous@fire-swamp.com"
+
+    click_button "Add Me"
+
+    expect(page).to have_content "R.O.U.S. added successfully"
+  end
+
+  scenario "user does not provide proper information for a monster" do
+
+    visit new_monster_path
+    expect(page).to have_content "Add a new monster!"
+
+    click_button "Add Me"
+    expect(page).to have_content "Name can't be blank"
+    expect(page).to have_content "Source can't be blank"
+    expect(page).to have_content "Image can't be blank"
+  end
+end
