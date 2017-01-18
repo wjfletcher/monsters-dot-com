b96440569be163c52e2829245d637ce9fb06bed5
Suzi Curran <suzicurran@gmail.com>
2017-01-18 16:55:10 -0500

    updated tests 7 and 8 to incorporate required user ID

--- spec/features/07_user_edits_an_existing_monster_spec.rb
+++ spec/features/07_user_edits_an_existing_monster_spec.rb
@@ -1,9 +1,11 @@
 require "rails_helper"
 
 feature "visitor goes to edit page for a particular monster" do
-  scenario "clicks link from show page" do
+  scenario "and clicks link from show page" do
 
-    monster_2 = Monster.create(name: 'Napstablook', source: 'http://undertale.wikia.com/wiki/Napstablook', img: 'https://cdn.thingiverse.com/renders/2f/f2/a6/b2/c5/blooky_preview_featured.jpg', description: 'Napstablook, known in the UnderNet as NAPSTABLOOK22, is a melancholic ghost monster and musician/DJ who lives in Waterfall, encountered in the Ruins as Undertale\'s first miniboss.', email: 'Napstablook22@undernet.com')
+    user_1 = User.create(email: "user@example.com", password: "password")
+
+    monster_2 = Monster.create(user_id: user_1.id, name: 'Napstablook', source: 'http://undertale.wikia.com/wiki/Napstablook', img: 'https://cdn.thingiverse.com/renders/2f/f2/a6/b2/c5/blooky_preview_featured.jpg', description: 'Napstablook, known in the UnderNet as NAPSTABLOOK22, is a melancholic ghost monster and musician/DJ who lives in Waterfall, encountered in the Ruins as Undertale\'s first miniboss.', email: 'Napstablook22@undernet.com')
 
     visit root_path
     click_link monster_2.name
