74bacde31f9b692d924aa0c9defb7fd0729ad334
Suzi Curran <suzicurran@gmail.com>
2017-01-19 13:43:59 -0500

    refactor monster params to match reviews

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -1,10 +1,11 @@
 class MonstersController < ApplicationController
+  before_action :set_monster, only: [:show, :edit, :update, :destroy]
+
   def index
     @monsters = Monster.all
   end
 
   def show
-    @monster = Monster.find(params[:id])
     # @reviews = @monster.reviews
   end
 
@@ -25,11 +26,9 @@ class MonstersController < ApplicationController
   end
 
   def edit
-    @monster = Monster.find(params[:id])
   end
 
   def update
-    @monster = Monster.find(params[:id])
 
     if @monster.update(monster_params)
       redirect_to @monster, notice: (@monster.name + " was successfully updated.")
@@ -39,7 +38,6 @@ class MonstersController < ApplicationController
   end
 
   def destroy
-    @monster = Monster.find(params[:id])
     name = @monster.name
     # @monster.reviews = nil
     @monster.destroy
@@ -47,6 +45,10 @@ class MonstersController < ApplicationController
   end
 
   private
+
+  def set_monster
+    @monster = Monster.find(params[:id])
+  end
 
   def monster_params
     params.require(:monster).permit(
