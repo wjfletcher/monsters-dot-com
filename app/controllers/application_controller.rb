63e55dba147b8902d20b11408112a6debf1ced32
wjfletcher <warrenjfletcher@gmail.com>
2017-01-19 16:40:08 -0500

    Add username to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    update_attrs = [:password, :password_confirmation, :current_password, :email]
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 end
