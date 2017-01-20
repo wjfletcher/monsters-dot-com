d0c89dcc1ac493167fe468e9260c2a75e9b7b83a
wjfletcher <warrenjfletcher@gmail.com>
2017-01-20 14:48:12 -0500

    shorten line for hound

--- spec/features/08_user_deletes_a_monster_spec.rb
+++ spec/features/08_user_deletes_a_monster_spec.rb
@@ -9,7 +9,8 @@ feature "user deletes monster from its edit page" do
       user_id: user1.id,
       name: 'Napstablook', source: 'http://undertale.wikia.com/wiki/Napstablook',
       img: 'https://cdn.thingiverse.com/renders/2f/f2/a6/b2/c5/blooky_preview_featured.jpg',
-      description: 'Napstablook, is a melancholic ghost monster and musician/DJ who lives in Waterfall.',
+      description: 'Napstablook, is a melancholic ghost monster and musician/DJ
+      who lives in Waterfall.',
       email: 'Napstablook22@undernet.com'
     )
 
