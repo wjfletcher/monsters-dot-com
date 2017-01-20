eebb1620c5024569e2fbc4e15f72774d17edb3b5
wjfletcher <warrenjfletcher@gmail.com>
2017-01-20 14:30:57 -0500

    kill the hound

--- spec/features/02_visitor_sees_monster_show_page_spec.rb
+++ spec/features/02_visitor_sees_monster_show_page_spec.rb
@@ -5,10 +5,23 @@ feature "visitor sees details for a particular monster" do
 
     user1 = User.create(username: "user", email: "user@example.com", password: "password")
 
-    monster1 = Monster.create(user_id: user1.id, name: 'Durin\'s Bane', source: 'http://lotr.wikia.com/wiki/Durin\'s_Bane', img: 'http://static.comicvine.com/uploads/original/5/56730/1086678-balrog2apj.jpg', description: 'Durin\'s Bane refers to the particular Balrog of Morgoth that had fled following Morgoth\'s defeat during the War of Wrath to live and slumber deep within the depths of the Dwarven kingdom of Moria during the Third Age.', email: 'Shadow_and_flame@moria.net')
-    monster2 = Monster.create(user_id: user1.id, name: 'Napstablook', source: 'http://undertale.wikia.com/wiki/Napstablook', img: 'https://cdn.thingiverse.com/renders/2f/f2/a6/b2/c5/blooky_preview_featured.jpg', description: 'Napstablook, known in the UnderNet as NAPSTABLOOK22, is a melancholic ghost monster and musician/DJ who lives in Waterfall, encountered in the Ruins as Undertale\'s first miniboss.', email: 'Napstablook22@undernet.com')
-    
+    monster1 = Monster.create(
+      user_id: user1.id,
+      name: 'Durin\'s Bane',
+      source: 'http://lotr.wikia.com/wiki/Durin\'s_Bane',
+      img: 'http://static.comicvine.com/uploads/original/5/56730/1086678-balrog2apj.jpg',
+      description: 'Durin\'s Bane refers to the particular Balrog of Morgoth that had fled following Morgoth\'s defeat during the War of Wrath to live and slumber deep within the depths of the Dwarven kingdom of Moria during the Third Age.',
+      email: 'Shadow_and_flame@moria.net')
+    monster2 = Monster.create(
+      user_id: user1.id,
+      name: 'Napstablook',
+      source: 'http://undertale.wikia.com/wiki/Napstablook',
+      img: 'https://cdn.thingiverse.com/renders/2f/f2/a6/b2/c5/blooky_preview_featured.jpg',
+      description: 'Napstablook, known in the UnderNet as NAPSTABLOOK22, is a melancholic ghost monster and musician/DJ who lives in Waterfall, encountered in the Ruins as Undertale\'s first miniboss.',
+      email: 'Napstablook22@undernet.com')
+
     visit root_path
+    expect(page).to have_content monster1.name
     click_link monster2.name
 
     expect(page).to have_content monster2.name
--- spec/features/08_user_deletes_a_monster_spec.rb
+++ spec/features/08_user_deletes_a_monster_spec.rb
@@ -5,7 +5,12 @@ feature "user deletes monster from its edit page" do
 
     user1 = User.create(username: "user", email: "user@example.com", password: "password")
 
-    monster2 = Monster.create(user_id: user1.id, name: 'Napstablook', source: 'http://undertale.wikia.com/wiki/Napstablook', img: 'https://cdn.thingiverse.com/renders/2f/f2/a6/b2/c5/blooky_preview_featured.jpg', description: 'Napstablook, known in the UnderNet as NAPSTABLOOK22, is a melancholic ghost monster and musician/DJ who lives in Waterfall, encountered in the Ruins as Undertale\'s first miniboss.', email: 'Napstablook22@undernet.com')
+    monster2 = Monster.create(
+      user_id: user1.id,
+      name: 'Napstablook', source: 'http://undertale.wikia.com/wiki/Napstablook',
+      img: 'https://cdn.thingiverse.com/renders/2f/f2/a6/b2/c5/blooky_preview_featured.jpg',
+      description: 'Napstablook, known in the UnderNet as NAPSTABLOOK22, is a melancholic ghost monster and musician/DJ who lives in Waterfall, encountered in the Ruins as Undertale\'s first miniboss.',
+      email: 'Napstablook22@undernet.com')
 
     visit root_path
     click_link monster2.name
