fe55215ad4aebd11a2b2291d2aea1d9071268503
LisaHernandez63 <Lisaleesa63@gmail.com>
2017-01-23 10:01:38 -0500

    removed user controller files that were not being used

--- app/controllers/users_controller.rb
+++
@@ -1,9 +0,0 @@
-class UsersController < Devise::RegistrationsController
-
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    redirect_to users_url, notice: 'User was successfully deleted.'
-  end
-
-end
