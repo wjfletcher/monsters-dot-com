d0bd7ec9273ae81fc1a2e89df7302e8aac18a2c6
Suzi Curran <suzicurran@gmail.com>
2017-01-25 14:07:30 -0500

    even more edits for the hound

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -6,65 +6,65 @@ class Monster < ApplicationRecord
   def grossness
     my_reviews = Review.where(monster_id: self.id)
     total = 0
-    unless my_reviews.length == 0
+    if my_reviews.empty?
+      "?"
+    else
       my_reviews.each do |review|
         total += review.grossness
       end
-      my_score = (total / my_reviews.length)
-    else
-      my_score = "?"
+      (total / my_reviews.length)
     end
   end
 
   def scariness
     my_reviews = Review.where(monster_id: self.id)
     total = 0
-    unless my_reviews.length == 0
+    if my_reviews.empty?
+      "?"
+    else
       my_reviews.each do |review|
         total += review.scariness
       end
-      my_score = (total / my_reviews.length)
-    else
-      my_score = "?"
+      (total / my_reviews.length)
     end
   end
 
   def cleverness
     my_reviews = Review.where(monster_id: self.id)
     total = 0
-    unless my_reviews.length == 0
+    if my_reviews.empty?
+      "?"
+    else
       my_reviews.each do |review|
         total += review.cleverness
       end
-      my_score = (total / my_reviews.length)
-    else
-      my_score = "?"
+      (total / my_reviews.length)
     end
   end
 
   def bigness
     my_reviews = Review.where(monster_id: self.id)
     total = 0
-    unless my_reviews.length == 0
+    if my_reviews.empty?
+      "?"
+    else
       my_reviews.each do |review|
         total += review.bigness
       end
-      my_score = (total / my_reviews.length)
-    else
-      my_score = "?"
+      (total / my_reviews.length)
     end
   end
 
   def badness
     my_reviews = Review.where(monster_id: self.id)
     total = 0
-    unless my_reviews.length == 0
+    if my_reviews.empty?
+      "?"
+    else
       my_reviews.each do |review|
         total += review.badness
       end
-      my_score = (total / my_reviews.length)
-    else
-      my_score = "?"
+      (total / my_reviews.length)
     end
   end
 
--- spec/features/16_user_searches_for_monster_by_attr_spec.rb
+++ spec/features/16_user_searches_for_monster_by_attr_spec.rb
@@ -3,17 +3,26 @@ require "rails_helper"
 feature "visitor runs a search from home page and sees results" do
   scenario "visits home and selects a search" do
     user1 = User.create(
-      email: "user@example.com", password: "password", username: "Cool User"
+      email: "user@example.com",
+      password: "password",
+      username: "Cool User"
     )
     user2 = User.create(
-      email: "user2@example.com", password: "password2", username: "User 2"
+      email: "user2@example.com",
+      password: "password2",
+      username: "User 2"
     )
     suzi = User.create(
-      email: "suzi@fakeemail.com", password: "PASSWORD", username: "Suzi Curran", admin: true
+      email: "suzi@fakeemail.com",
+      password: "PASSWORD",
+      username: "Suzi Curran",
+      admin: true
     )
 
     balrog = Monster.create(
-      user_id: user1.id, name: 'Durin\'s Bane', source: 'http://lotr.wikia.com/wiki/Durin\'s_Bane',
+      user_id: user1.id,
+      name: 'Durin\'s Bane',
+      source: 'http://lotr.wikia.com/wiki/Durin\'s_Bane',
       img: 'http://static.comicvine.com/uploads/original/5/56730/1086678-balrog2apj.jpg',
       description: 'Durin\'s Bane refers to the particular Balrog of Morgoth that
       had fled following Morgoth\'s defeat during the War of Wrath to live and slumber
@@ -22,21 +31,26 @@ feature "visitor runs a search from home page and sees results" do
     )
 
     blooky = Monster.create(
-      user_id: user1.id, name: 'Napstablook',
+      user_id: user1.id,
+      name: 'Napstablook',
       source: 'http://undertale.wikia.com/wiki/Napstablook',
       img: 'https://cdn.thingiverse.com/renders/2f/f2/a6/b2/c5/blooky_preview_featured.jpg',
       description: 'Napstablook, known in the UnderNet as NAPSTABLOOK22, is a
       melancholic ghost monster and musician/DJ who lives in Waterfall, encountered
-      in the Ruins as Undertale\'s first miniboss.', email: 'Napstablook22@undernet.com'
+      in the Ruins as Undertale\'s first miniboss.',
+      email: 'Napstablook22@undernet.com'
     )
 
-    cookiemonster = Monster.create(user_id: user1.id, name: 'Cookie Monster',
+    cookiemonster = Monster.create(
+      user_id: user1.id,
+      name: 'Cookie Monster',
       source: 'https://en.wikipedia.org/wiki/Cookie_Monster',
       img: 'http://www.grindtv.com/wp-content/uploads/2013/09/cookiemonster.jpeg',
       description: 'Cookie Monster is a Muppet on the long-running children\'s
       television show Sesame Street. He is best known for his voracious appetite
       and his famous eating phrases, such as "Me want cookie!" "Me eat cookie!"
-      and "Om nom nom nom".', email: 'mewantcookie@sstreet.org'
+      and "Om nom nom nom".',
+      email: 'mewantcookie@sstreet.org'
     )
 
     Review.create(
