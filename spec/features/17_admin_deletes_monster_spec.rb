0f9f173dfbbff665ca2a75d2d09598fc95b1e25c
wjfletcher <warrenjfletcher@gmail.com>
2017-01-25 15:07:31 -0500

    hound

--- spec/features/17_admin_deletes_monster_spec.rb
+++ spec/features/17_admin_deletes_monster_spec.rb
@@ -36,9 +36,9 @@ feature "Any monster" do
 
     User.create(username: "admin", email: "admin@example.com", password: "password", admin: true)
     user1 = User.create(username: "Sam", email: "sam@example.com", password: "password")
-    user2 = User.create(username: "Bob", email: "bob@example.com", password: "password")
+    User.create(username: "Bob", email: "bob@example.com", password: "password")
 
-    monster2 = Monster.create(
+    Monster.create(
       user_id: user1.id,
       name: 'Napstablook', source: 'http://undertale.wikia.com/wiki/Napstablook',
       img: 'https://cdn.thingiverse.com/renders/2f/f2/a6/b2/c5/blooky_preview_featured.jpg',
