38afc5475441a09115c4a26d745ecf4037f96a3b
Suzi Curran <suzicurran@gmail.com>
2017-01-26 11:33:10 -0500

    add sendgird to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "monsters-dot-com_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'radiant-atoll-34822.herokuapp.com',
+    :authentication => :plain
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
