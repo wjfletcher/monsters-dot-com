ab80ca863e3c3398e38a7b19e039d85a6b892452
Jonathan Braun <Jonathanrbraun@gmail.com>
2017-01-27 13:43:24 -0500

    desc now more organized

--- db/schema.rb
+++ db/schema.rb
@@ -56,8 +56,8 @@ ActiveRecord::Schema.define(version: 20170126223250) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.string   "username",                               null: false
-    t.string   "avatar"
     t.boolean  "admin",                  default: false
+    t.string   "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
